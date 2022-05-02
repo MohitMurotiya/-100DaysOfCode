dcad75ea811636c621007e1f3226d4c2bd2bd66c
MM <mohitmurotiya98@gmail.com>
2022-05-02 23:06:54 +0530

    audio logic added

---
+++ Day 1/index.js
@@ -0,0 +1,19 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    
+    key.classList.add('playing');
+    audio.currentTime = 0; //It'll play audio from start
+    audio.play();
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+document.addEventListener('keydown', playSound);
